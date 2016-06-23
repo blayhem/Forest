f08bd2e69c92ccb1adf69750eed1528aac2c1ece
Daniel Fernandez Rico <deferrico@gmail.com>
2016-06-23 11:40:49 +0200

    Minor changes 🔨

--- forest.py
+++ forest.py
@@ -23,42 +23,45 @@ def isTree(x,y):
     return getCoordCont(x,y)[0] == 'T'
 
 def setFire():
-    x = r.randrange(0,20,1)
-    y = r.randrange(0,20,1)
+    x = r.randrange(3,13,1)
+    y = r.randrange(3,23,1)
     elem = getCoordCont(x,y)
     if elem[0] == 'T':
         trees[elem[1]] = (x,y,'F')
+    #     return (x,y)
+    # else:
+    #     return (0,0)
 
 def hasTreesAround(x,y):
     return isTree(x,y+1) and isTree(x,y-1) and isTree(x+1,y) and isTree(x-1,y)
 
 def pbar(window):
-    trees.insert(0,(10,10,"T"))
+    trees.insert(0,(8,8,"T"))
     for i in range(50):
     #while not trees:
-        # if i%5==0:
-        #     setFire()
+        if i%5==0:
+             setFire()
         for tree in trees:
             x = tree[0]
             y = tree[1]
-            if hasTreesAround(x,y):
+            if hasTreesAround(x,y) and getCoordCont(x,y)[0]!='F':
                 trees.remove(tree)
+                window.addstr(x,y, " ")
                 # tree = [x for x in trees if x != tree]
             else:
                 posNew = getRandomTree(x,y)
-                while (posNew[0]<3 or posNew[1]<3):
+                while (posNew[0]<3 or posNew[1]<3 or posNew[0]>13 or posNew[1]>23):
                     posNew = getRandomTree(x,y)
-                xb = posNew[0]
-                yb = posNew[1]
-                if not hasTreesAround(xb,yb):
+                (xb,yb) = posNew
+                if not hasTreesAround(xb,yb) and getCoordCont(xb,yb)[0]!='F':
                     bufferTrees.insert(0,(xb,yb,"T"))
 
                 window.addstr(x,y, tree[2])
         #window.addstr(10, 10, "[" + ("=" * i) + ">" + (" " * (10 - i )) + "]")
         #window.addstr(11, 10, "This is test"+ str(i+1))
         for tree in bufferTrees:
-            trees.insert(0,tree)
+            trees.insert(len(trees),tree)
         window.refresh()
-        time.sleep(0.5)
+        if i<10: time.sleep(0.1)
 
 curses.wrapper(pbar)
